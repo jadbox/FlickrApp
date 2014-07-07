24681b559ae1812f43ce93733e119d90dcf900c0
Jonathan Dunlap <jadit2@gmail.com>
2014-07-06 19:58:02 -0700

    Init

---
+++ WpfApplication1/Flickr.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using Newtonsoft.Json;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Flickr
+    {
+        public Flickr()
+        {
+            
+        }
+
+        private string flickerRU(String args)
+        {
+            return "https://api.flickr.com/services/rest/?format=json" +
+                    "&nojsoncallback=1&api_key=256663858aa10e52a838a58b7866d858" + args;
+        }
+
+        private string tagsRU(String tags)
+        {
+            return flickerRU("&method=flickr.photos.search&sort=random&per_page=10&tags=" + tags);
+        }
+
+        private string getOne(String id)
+        {
+            return flickerRU("&method=flickr.photos.getSizes&photo_id=" + id);
+        }
+
+        public String getImageUrlForTag(String imageTag)
+        {
+            WebClient webClient = new WebClient();
+            String json = webClient.DownloadString(tagsRU(Uri.EscapeUriString(imageTag)));
+            dynamic a = JsonConvert.DeserializeObject(json);
+            String id = a.photos.photo[1].id;
+            Console.Out.WriteLine(id);
+
+            String jsonImage = webClient.DownloadString(getOne(id));
+            Console.Out.WriteLine(jsonImage);
+
+            dynamic b = JsonConvert.DeserializeObject(jsonImage);
+
+            String source = b.sizes.size[0].source;
+            Console.Out.WriteLine(source);
+            return source;
+        }
+
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CommonCommandBinding_CanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+            e.CanExecute = true;
+        }
+
+        private string flickerRU(String args)
+        {
+            return "https://api.flickr.com/services/rest/?format=json" +
+                    "&nojsoncallback=1&api_key=256663858aa10e52a838a58b7866d858" + args;
+        }
+
+        private Flickr flickr = new Flickr();
+
+        private void CommonCommandBinding_Execute(object sender, ExecutedRoutedEventArgs e)
+        {
+            addt(flickr.getImageUrlForTag(searchFor.Text));
+        }
+
+        public void addt(String url) {
+            Image image = new Image();
+            image.BeginInit();
+            image.Source = new BitmapImage(new Uri(url));
+            image.EndInit();
+            image.Width = 100;
+            image.Height = 100;
+            image.Stretch = Stretch.Uniform;
+            image.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+            image.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+            myg.Children.Add(image);
+        }
+    }
+}
